8472af647165d1608e87c31c3e232cced83bf227
tamargirl25 <nanuashvilitamara@gmail.com>
2024-10-15 00:18:49 +0400

    commit

--- Level 04/task1.py
+++ Level 04/task1.py
@@ -1,8 +1,29 @@
 #1 მომხმარებელს შემოატანინე თავისი სახელი, შემდეგ შემატანინეთ მეგობრის გვარი და დაბეჭდეთ ისინი კონკატინაციის მეშვეობით.
-name=(input("enter youre name: "))
-name1=(input("enter youre freand name: "))
+name=(input("enter your name: "))
+name1=(input("enter your friend name: "))
 print(name,name1)
 
 #2 მომხმარებელს შემოატანინეთ მისი ასაკი და დაუბეჭდეთ რამდენი წლის იქნება 10 წელში.
-age=int(input("enter youre age: "))
-print(age+10) 
+age=int(input("enter your age: "))
+print(age+10)
+
+#3 მომხმარებელს შემოატანინეთ 5 რიცხვი და დაითვალეთ ამ რიცხვების საშუალო არითმეტიკული.
+number1=int(input("enter your first number: "))
+number2=int(input("enter your second number: "))
+number3=int(input("enter your third number: "))
+number4=int(input("enter your forth number: "))
+number5=int(input("enter your fifth number: "))
+average=(number1+number2+number3+number4+number5)/5
+print("The arithmetic average of the numbers is: ", average)
+
+#4 კომენტარის სახით ახსენით რა არის case sansitive + რა არის snake case.
+# case sansitive ნიშნავს დიდი და პატარა დაწერილ აცოებს ანეჭიბა მნიშვნელობას.
+# snake case არის გამოყენების კონვენცია გამოთვლებში, ცვლადების და ქვეპროგრამის სახელებისთვის და ფაილების სახელებისთვის.
+
+#5 დაუშვით 5 შეცდომა და გვერდით კომენტარის სახით მიუწერეთ სწორი ვერსია (ახსენით რა არის debugging)
+print("The arithmetic average of the numbers is: ", Average)# ჩემს მიერ შექმნილი ცვლადი წერია არასწორად.
+age=(input("enter your age: "))# type არ არის მითითებული.
+name=(input(enter your name: ))# ფრჩხილებში არ არის პრჭყალები.
+number1=int(("enter your first number: "))# input აკლია.
+number5int(input("enter your fifth number: "))# = აკლია.
+# debugging არის შეცდომის ძირეული მიზეზის აღმოჩენის და შეცდომების შესაძლო გამოსწორების პროცესი.
