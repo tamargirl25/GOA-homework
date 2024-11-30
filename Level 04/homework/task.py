ad4376f12b0ff561f39b79d583bc0ef1d87d3376
tamargirl25 <nanuashvilitamara@gmail.com>
2024-11-30 17:04:56 +0400

    commit

--- Level 04/homework/task.py
+++ Level 04/homework/task.py
@@ -1,13 +1,13 @@
-#1 მომხმარებელს შემოატანინე თავისი სახელი, შემდეგ შემატანინეთ მეგობრის გვარი და დაბეჭდეთ ისინი კონკატინაციის მეშვეობით.
+#1
 name=(input("enter your name: "))
 name1=(input("enter your friend name: "))
 print(name,name1)
 
-#2 მომხმარებელს შემოატანინეთ მისი ასაკი და დაუბეჭდეთ რამდენი წლის იქნება 10 წელში.
+#2
 age=int(input("enter your age: "))
 print(age+10)
 
-#3 მომხმარებელს შემოატანინეთ 5 რიცხვი და დაითვალეთ ამ რიცხვების საშუალო არითმეტიკული.
+#3
 number1=int(input("enter your first number: "))
 number2=int(input("enter your second number: "))
 number3=int(input("enter your third number: "))
@@ -16,11 +16,11 @@ number5=int(input("enter your fifth number: "))
 average=(number1+number2+number3+number4+number5)/5
 print("The arithmetic average of the numbers is: ", average)
 
-#4 კომენტარის სახით ახსენით რა არის case sansitive + რა არის snake case.
+#4
 # case sansitive ნიშნავს დიდი და პატარა დაწერილ აცოებს ანეჭიბა მნიშვნელობას.
 # snake case არის ცვლადის სახელის დაწერის წესი, სადაც პატარა ასოებით სწერთ და ქვედატირეთი ვყოფთ.
 
-#5 დაუშვით 5 შეცდომა და გვერდით კომენტარის სახით მიუწერეთ სწორი ვერსია (ახსენით რა არის debugging)
+#5
 #print("The arithmetic average of the numbers is: ", Average)# ჩემს მიერ შექმნილი ცვლადი წერია არასწორად.
 #age=(input("enter your age: "))# type არ არის მითითებული.
 #name=(input(enter your name: ))# ფრჩხილებში არ არის პრჭყალები.
