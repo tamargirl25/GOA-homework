fb5afb78adb5b69b9ea2af633ae9d904e019bbc4
tamargirl25 <nanuashvilitamara@gmail.com>
2024-12-17 19:49:42 +0400

    commit

---
+++ Level 03/homework/task.py
@@ -0,0 +1,38 @@
+#1
+print(5 + 5)
+print(10 + 10)
+print(3 + 3)
+print(7 + 7)
+print(15 + 15)
+
+print(10 - 2)
+print(8 - 4)
+print(5 - 1)
+print(17 - 7)
+print(9 - 3)
+
+print(9 * 9)
+print(5 * 5)
+print(9 * 3)
+print(12 * 7)
+print(5 * 1)
+
+print(4 / 2)
+print(9 / 3)
+print(15 / 3)
+print(20 /10)
+print(6 / 2)
+
+#2
+age=15
+age1=15 + 5
+print(age,age1)
+
+#3
+age=15, 17, 47, 49
+print(15+17+47+49)
+
+#4
+name="tamari"
+name="barbare"
+print(name)
